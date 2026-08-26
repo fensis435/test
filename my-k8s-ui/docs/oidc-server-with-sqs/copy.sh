7df2c3e2920c3e846ed543a1c4ed827f2bd2f943
fensis435 <jet.hut+github@gmail.com>
2026-08-27 08:10:49 +0900

    wip

--- my-k8s-ui/docs/oidc-server-with-sqs/copy.sh
+++
@@ -1,3 +0,0 @@
-mv -i /mnt/g/Downloads/oidc-*.zip .
-chmod -x oidc-*.zip
-scp oidc-*.zip vaio@192.168.0.36:~/oidc-server-with-sqs/
--- my-k8s-ui/docs/oidc-server-with-sqs/sync.sh
+++
@@ -1,3 +0,0 @@
-#rsync -av --update vaio@192.168.0.36:~/oidc-server-with-sqs/ .
-set -x
-rsync -avi --update --dry-run vaio@192.168.0.36:~/oidc-server-with-sqs/ .
